d865ca49d39f3473ca0320863162dc1cea16937d
Nadejda <cthuthbev@gmail.com>
2022-02-05 20:19:48 +0300

    Vibration, message

--- app/src/main/java/com/example/hw17timer/MainActivity.kt
+++ app/src/main/java/com/example/hw17timer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.hw17timer
 
+import android.content.Context
+import android.os.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.CountDownTimer
+import androidx.appcompat.app.AlertDialog
 import com.example.hw17timer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,39 +12,91 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var timer: CountDownTimer? = null
-    var seconds = 0
+    private var secondsNow = ""
+    var flag = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
-            btTimer.setOnClickListener(){
-                if(binding.tvTimer.text.toString().isNotEmpty()){
-                    seconds = Integer.valueOf(binding.edTimeForTimer.text.toString())
-                    timerFun()
+            btTimer.setOnClickListener() {
+                when (flag) {
+                    0 -> secondsNow = binding.edTimeForTimer.text.toString()+"000"
+                    1 -> secondsNow = binding.tvTimer.text.toString()
+                }
+                Toast.makeText(baseContext,"Закончился через " + secondsNow + " миллисекунд",Toast.LENGTH_SHORT).show()
+                startCountDawnTimer(secondsNow)
+            }
+            btCancel.setOnClickListener {
+                flag = 0
+                cancelCountDawnTimer()
+            }
+
+            btPause.setOnClickListener {
+                flag = 1
+                pauseCountDawnTimer()
+            }
+        }
+    }
+
+    private fun startCountDawnTimer(time: String){
+        timer?.cancel()
+        try {
+            var seconds = Integer.valueOf(time)
+            timer = object : CountDownTimer((seconds).toLong(), 1){
+
+                override fun onTick(time: Long) {
+                    binding.tvTimer.text = time.toString()
+                }
+
+                override fun onFinish() {
+                    binding.tvTimer.text = "Finish!"
+                    flag = 0
+                    viber(this@MainActivity)
+                }
+            }.start()
+        } catch (e: Exception) {
+            binding.tvTimer.text = "Error"
+        }
+    }
+
+    private fun cancelCountDawnTimer(){
+        val alterDialog = AlertDialog.Builder(this)
+        alterDialog.setTitle("Cancel?")
+        alterDialog.setMessage("Are u sure?")
+        alterDialog.setPositiveButton("Yep") {_,_ ->
+            timer?.cancel()
+            binding.tvTimer.text = "Cancel"
+            flag = 0
+        }
+        alterDialog.setNegativeButton("NO"){_,_ -> }
+        alterDialog.show()
+    }
+
+    private fun pauseCountDawnTimer(){
+        timer?.cancel()
+    }
+
+    companion object {
+        fun viber(context: Context) {
+            val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            val canVibrate: Boolean = vibrator.hasVibrator()
+            val milliseconds = 1000L
+
+            if (canVibrate) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrator.vibrate(
+                        VibrationEffect.createOneShot(
+                            milliseconds,
+                            VibrationEffect.DEFAULT_AMPLITUDE
+                        )
+                    )
+                } else {
+                    vibrator.vibrate(milliseconds)
                 }
             }
         }
-    }
-
-    private fun timerFun() {
-        if(binding.tvTimer.text.toString().isNotEmpty()){
-            seconds = Integer.valueOf(binding.edTimeForTimer.text.toString())
-            startCountDawnTimer(seconds)
-        }
-    }
-
-    private fun startCountDawnTimer(timeMillis: Int){
-        timer?.cancel()
-        timer = object : CountDownTimer((timeMillis*1000).toLong(), 1){
-
-            override fun onTick(time: Long) {
-                binding.tvTimer.text = time.toString()
-            }
-
-            override fun onFinish() {
-                binding.tvTimer.text = "Finish!"
-            }
-        }.start()
     }
 }
